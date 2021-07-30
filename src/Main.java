d0bf1c0e29ac47280b1c2e7f8378e002edc83d65
이지우 <ljiwoo59@gmail.com>
2021-07-30 16:23:31 +0900

    happy_house

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import happyhouse.view.HouseInfoView;
+
+public class Main {
+	public static void main(String[] args) {
+		new HouseInfoView();
+	}
+}
---
+++ src/util/HouseDealSAXHandler.java
@@ -0,0 +1,69 @@
+package happyhouse.util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+import happyhouse.model.dto.HouseDeal;
+
+/**
+ *  HouseDealHistory.xml 파일에서 주택 거래 정보를 읽어 파싱하는 핸들러 클래스 
+ */
+public class HouseDealSAXHandler extends DefaultHandler {
+	private List<HouseDeal> houses;
+	private HouseDeal house;
+	private String temp;
+	
+	public HouseDealSAXHandler(){
+		houses = new LinkedList<HouseDeal>();
+	}
+	
+	public void startElement(String uri, String localName, String qName, Attributes att ){
+		if(qName.equals("item")){
+			house = new HouseDeal(HouseSaxParser.no++);
+			house.setType(HouseDeal.HOUSE_DEAL);
+			houses.add(house);
+		}
+	}
+	public void endElement(String uri, String localName, String qName){
+		if(qName.equals("지역코드")) { 
+			house.setCode(Integer.parseInt(temp));
+		}else if(qName.equals("연립다세대")) { 
+			house.setAptName(temp.trim());
+		}else if(qName.equals("법정동")) { 
+			house.setDong(temp.trim());
+		}else if(qName.equals("거래금액")) { 
+			house.setDealAmount(temp);
+		}else if(qName.equals("건축년도")) { 
+			house.setBuildYear(Integer.parseInt(temp));
+		}else if(qName.equals("년")) { 
+			house.setDealYear(Integer.parseInt(temp));
+		}else if(qName.equals("월")) { 
+			house.setDealMonth(Integer.parseInt(temp));
+		}else if(qName.equals("일")) { 
+			house.setDealDay(Integer.parseInt(temp));
+		}else if(qName.equals("전용면적")) { 
+			house.setArea(Double.parseDouble(temp));
+		}else if(qName.equals("지번")) { 
+			house.setJibun(temp);
+		}else if(qName.equals("층")) { 
+			house.setFloor(Integer.parseInt(temp));
+		}
+	}
+	public void characters(char[]ch, int start, int length){
+		temp = new String(ch, start, length);
+	}
+	public List<HouseDeal> getHouses() { 
+		return houses; 
+	}
+	public void setHouses(List<HouseDeal> houses) {
+		this.houses = houses;
+	}
+}
+
+
+
+
+
